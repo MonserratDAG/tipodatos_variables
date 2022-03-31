90c69bc1e7bfb164ad3eac0ef2df05656d63dacd
Monserrat Del Angel Guadarrama <monse.162000@hotmail.com>
2022-03-30 19:05:48 -0600

    Archivos Tipo de Datos y Variables

---
+++ variables.js
@@ -0,0 +1,32 @@
+//variables booleanas
+var res1= false;
+var res2= new Boolean(true);
+document.write("res1 = " + res1);
+document.write("<br>");
+document.write("res2 = " + res2);
+document.write("<br><br>");
+
+//variables array
+var miarray = ["mis colores favoritos son los pasteles"]; 
+document.write(miarray); 
+document.write("<br>");
+var miarray = [20, 33]; 
+document.write("El valor actual es " + miarray[1]);
+document.write("<br><br>");
+
+//variables de texto 
+var mitexto = "Hola Mundo!"; 
+document.write(mitexto);
+document.write("<br>");
+var mitexto = 'Esta es una cadena de texto'; 
+document.write(mitexto);
+document.write("<br><br>");
+
+//variables numericas 
+var minumero = 0; 
+minumero += 25; 
+document.write(minumero); 
+document.write("<br>");
+var minumero = 2; 
+minumero = minumero * (20 + 5);
+document.write(minumero); 
